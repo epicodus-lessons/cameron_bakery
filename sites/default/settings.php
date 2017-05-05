597bff9cd244cff32674960a4a1d9cb44de71373
sarah leahy <sarah leahy>
2017-05-05 17:30:57 -0600

    change background, logo, font and favicon on subtheme

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -258,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'qYFo-OXl4X582g_Yw7UNEaMYnrvtSLrKXlMZ-HLYRyI';
+$drupal_hash_salt = 'up7c6lIhA27wI8WbhJxgzQYoWuc3VLDDgxQlXSTb3Yk';
 
 /**
  * Base URL (optional).
