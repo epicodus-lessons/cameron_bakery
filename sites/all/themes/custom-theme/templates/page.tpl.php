f4877c6a50516dc9e06403709a1833db1f0d7f51
Katie Reed <katharynreedpierce@gmail.com>
2017-05-05 16:19:25 -0700

    add initial styling to custom theme

--- sites/all/themes/custom-theme/templates/page.tpl.php
+++ sites/all/themes/custom-theme/templates/page.tpl.php
@@ -79,7 +79,7 @@
 
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          <img src="https://cdn1.iconfinder.com/data/icons/minimal-fruit/128/strawberry-128.png" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
 
