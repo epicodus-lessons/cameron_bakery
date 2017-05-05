1546e422f6195d4e5a48c1e471d034b48aa553fa
Jayeson Kiyabu <jkiyabu@comast.net>
2017-05-05 16:20:48 -0700

    make fixes for custom themes

--- sites/all/themes/zen/templates/html.tpl.php
+++ sites/all/themes/zen/templates/html.tpl.php
@@ -9,31 +9,31 @@
 ?><!DOCTYPE html>
 <html <?php print $html_attributes . $rdf_namespaces; ?>>
 <head>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
 
-  <?php if ($default_mobile_metatags): ?>
-    <meta name="MobileOptimized" content="width">
-    <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width">
-  <?php endif; ?>
+    <?php if ($default_mobile_metatags): ?>
+        <meta name="MobileOptimized" content="width">
+        <meta name="HandheldFriendly" content="true">
+        <meta name="viewport" content="width=device-width">
+    <?php endif; ?>
 
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-  <?php if ($add_html5_shim): ?>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+    <?php if ($add_html5_shim): ?>
     <!--[if lt IE 9]>
     <script src="<?php print $base_path . $path_to_zen; ?>/js/html5shiv.min.js"></script>
     <![endif]-->
-  <?php endif; ?>
+    <?php endif; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <?php if ($skip_link_text && $skip_link_anchor): ?>
-    <p class="skip-link__wrapper">
-      <a href="#<?php print $skip_link_anchor; ?>" class="skip-link visually-hidden visually-hidden--focusable" id="skip-link"><?php print $skip_link_text; ?></a>
-    </p>
-  <?php endif; ?>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
+    <?php if ($skip_link_text && $skip_link_anchor): ?>
+        <p class="skip-link__wrapper">
+            <a href="#<?php print $skip_link_anchor; ?>" class="skip-link visually-hidden visually-hidden--focusable" id="skip-link"><?php print $skip_link_text; ?></a>
+        </p>
+    <?php endif; ?>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
 </body>
 </html>
