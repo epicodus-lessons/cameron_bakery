93fb52e3bac2a86324c03a921f31a19ec3b2d255
Jason Brown <jasontbrown99@gmail.com>
2017-05-05 20:40:53 -0700

    add styling to evanthemefromscratch

--- sites/all/themes/evanScratchTheme/templates/page.tpl.php
+++ sites/all/themes/evanScratchTheme/templates/page.tpl.php
@@ -2,18 +2,24 @@
 <div id="page-wrapper">
     <header>
         <?php print render($page['header']); ?>
-        <div id="logo">
-            <!--logo-->
-            <?php if ($logo): ?>
-            <a href="<?php print $base_path; ?>" title="<?php print t('Click to return to the Homepage'); ?>"/>
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home');?>"/></a>
-        <?php endif; ?>
+
+
+        <div id="logo-wrapper">
+            <ul>
+                <li><div id="logo">
+                    <!--logo-->
+                    <?php if ($logo): ?>
+                    <a href="<?php print $base_path; ?>" title="<?php print t('Click to return to the Homepage'); ?>"/>
+                        <img src="<?php print $logo; ?>" alt="<?php print t('Home');?>"/></a>
+                    <?php endif; ?>
+                </div></li>
+                <li><h2>Cameron's Bakery</h2></li>
+                <li><h3>Cookies Galore</h3></li>
+            </ul>
+
         </div>
         <nav>
-            <div id="main-menu">
-            <?php print theme('links_system_main_menu', array('links' => $main_menu, 'attributes' => array('class'
-            => array('links','menu','inline','clearfix')))); ?>
-            </div>
+
             <div id="navigation">
                 <?php print render($page['navigation']); ?>
             </div>
@@ -26,23 +32,12 @@
         <?php print render($page['sidebar_second']); ?>
     </div>
     <article>
-        <!-- title -->
-        <?php print render($title_prefix); ?>
-            <?php if ($title): ?>
-            <h1>
-            <?php print $title; ?>
-            </h1>
-            <?php endif; ?>
-        <?php print render($title_suffix); ?>
+
         <?php print render($page['content']); ?>
-        <!-- tabs -->
-        <?php if ($tabs): ?>
-        <?php print render($tabs); ?>
-        Custom Theme by Jason Brown
-        <?php endif; ?>
+
         <!-- messages -->
         <?php print $messages; ?>
     </article>
-    <footer><?php print render($page['footer']); ?></footer>
     <?php print render($page['page_bottom']); ?>
+    <footer><?php print render($page['footer']); ?></footer>
 </div>
