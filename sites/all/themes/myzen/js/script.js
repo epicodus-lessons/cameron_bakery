17e272ab44804cbdbc1844c84035d9512532418e
felixop8 <felixop8@gmail.com>
2017-05-05 22:52:18 -0700

    add css

--- sites/all/themes/myzen/js/script.js
+++ sites/all/themes/myzen/js/script.js
@@ -19,7 +19,7 @@
     attach: function (context, settings) {
 
         jQuery(document).ready(function($) {
-          $('#navigation h2').click(function(){
+          $('h2').click(function(){
             alert('I am an h2!');
           });
         });
