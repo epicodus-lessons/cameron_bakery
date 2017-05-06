55a4eb31247a17115bbdca249b00854122ed6d8d
Erica Wright <ericaw21@gmail.com>
2017-05-05 23:59:06 -0700

    update final styling colors for cupcake buttons

--- sites/all/themes/pastels/js/scripts.js
+++ sites/all/themes/pastels/js/scripts.js
@@ -2,12 +2,12 @@ jQuery(document).ready(function($) {
   $('#green_cupcake').click(function(){
       $("html").css("background-color", "#64D182");
       $("html").css("background", "linear-gradient(#64D182, #77DAAD)");
-      $("#mainmenusection").css("background-color", "#77DA9E");
+      $("#mainmenusection").css("background-color", "#F7D496");
   });
   $('#blue_cupcake').click(function(){
-      $("html").css("background-color", "#93BAEA");
-      $("html").css("background", "linear-gradient(#93BAEA, #A3E6F0)");
-      $("#mainmenusection").css("background-color", "#B1EBF4");
+      $("html").css("background-color", "#76C4F0");
+      $("html").css("background", "linear-gradient(#76C4F0, #81EBF8)");
+      $("#mainmenusection").css("background-color", "#F7A396");
   });
   $('#purple_cupcake').click(function(){
       $("html").css("background-color", "#D6C1F9");
