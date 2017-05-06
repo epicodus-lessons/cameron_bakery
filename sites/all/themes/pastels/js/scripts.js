09bf627108b03a6fc506351e4700d3cf9de25f9c
Erica Wright <ericaw21@gmail.com>
2017-05-05 21:03:02 -0700

    add javascript to change page styling on cupcake click:

--- sites/all/themes/pastels/js/scripts.js
+++ sites/all/themes/pastels/js/scripts.js
@@ -1,11 +1,17 @@
 jQuery(document).ready(function($) {
   $('#green_cupcake').click(function(){
-    alert('I am a cupcake!');
+      $("html").css("background-color", "#64D182");
+      $("html").css("background", "linear-gradient(#64D182, #77DAAD)");
+      $("#mainmenusection").css("background-color", "#77DA9E");
   });
   $('#blue_cupcake').click(function(){
-    alert('I am a cupcake!');
+      $("html").css("background-color", "#93BAEA");
+      $("html").css("background", "linear-gradient(#93BAEA, #A3E6F0)");
+      $("#mainmenusection").css("background-color", "#B1EBF4");
   });
   $('#purple_cupcake').click(function(){
-    alert('I am a cupcake!');
+      $("html").css("background-color", "#D6C1F9");
+      $("html").css("background", "linear-gradient(#D6C1F9, #A798D8, #E38EEB)");
+      $("#mainmenusection").css("background-color", "#CFB7F9");
   });
 });
