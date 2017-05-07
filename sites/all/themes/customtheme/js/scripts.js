1595faa0e985f1ead54f0426ea75b171adac0ffc
mkelley2 <[m_kelley2@yahoo.com]>
2017-05-06 21:37:00 -0700

    add custom styling

---
+++ sites/all/themes/customtheme/js/scripts.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+  $('#navigation h2').click(function(){
+    alert('Congrats on finding the hidden easter egg! Add the coupoon code "EGG25" to get 25% off your next purchase!');
+  });
+});
