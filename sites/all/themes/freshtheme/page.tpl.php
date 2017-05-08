e483fae1e3963558aab88673c1caa34adc0d9982
Jay Freeman <jaythinkshappiness@gmail.com>
2017-05-07 22:21:19 -0700

    add readme with custom theme styling

--- sites/all/themes/freshtheme/page.tpl.php
+++ sites/all/themes/freshtheme/page.tpl.php
@@ -1,3 +1,45 @@
-<div id="love" class="custom-class">
-  <?php print render($page['love']); ?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+<nav>
+    <div id="navbars" class="nav-wrapper">
+      <a href="#" class="brand-logo right">Bakery</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="/">Home</a></li>
+      </ul>
+    </div>
+  </nav>
+
+<div class="container">
+<div class="row">
+<div class="col m2"></div>
+    <div class="col m8" id="love" class="custom-class">
+        <?php print render($page['love']); ?>
+    </div>
+<div class="col m2"></div>
+
+</div>
+<div class="row">
+<div class="col m2"></div>
+    <div class="col m8" id="content" class="custom-class">
+        <?php print render($page['content']); ?>
+    </div>
+<div class="col m2"></div>
+
+</div>
+<div id="help" class="custom-class">
+    <?php print render($page['help']); ?>
+</div>
+
+<div id="page_bottom" class="custom-class">
+    <?php print render($page['page_bottom']); ?>
+</div>
+<div id="page_top" class="custom-class">
+    <?php print render($page['page_top']); ?>
+</div>
+<div id="deals" class="custom-class">
+    <?php print render($page['deals']); ?>
 </div>
