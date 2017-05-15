659ab9193958e2491c69e32545ea340fa32c97d2
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-05-15 07:14:39 -0700

    rename custom zen theme and change font.

---
+++ sites/all/themes/Custom/page.tpl.php
@@ -0,0 +1,49 @@
+<div id="root">
+
+    <div class="header-wrap">
+        <div class="header">
+            <div class="logo">
+                <?php print '<a href="/"><img src="'.$logo.'" alt="'.variable_get('site_name', '').'"></a>' ?>
+            </div>
+            <?php print render($page['title']); ?>
+            <?php print render($page['header']); ?>
+        </div>
+    </div>
+
+    <div class="nav-wrap">
+        <div class="nav">
+            <?php print theme('links', array('links' => $main_menu)); ?>
+        </div>
+    </div>
+
+    <div class="body-wrap">
+        <div class="body">
+            <div class="content_header">
+                <?php print render($page['content_header']) ?>
+            </div>
+            <div class="content">
+                <?php print render($page['content']); ?>
+            </div>
+
+            <div class="sidebar-first">
+                <?php print render($page['sidebar_first']); ?>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="sidebar-second-wrap">
+        <div class="sidebar-second">
+            <?php print render($page['sidebar_second']); ?>
+        </div>
+    </div>
+
+    <div class="footer-wrap">
+        <div class="footer">
+            <?php print render($page['footer']); ?>
+        </div>
+    </div>
+
+</div>
+<!-- root -->
