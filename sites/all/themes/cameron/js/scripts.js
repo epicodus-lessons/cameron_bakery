91cfe57ebe9309ef3c001cea850de6f917bd4437
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-05-15 09:00:17 -0700

    edit scripts to alert a message when logo/header is clicked.

--- sites/all/themes/cameron/js/scripts.js
+++ sites/all/themes/cameron/js/scripts.js
@@ -1,3 +1,5 @@
-jQuery(document).ready(function() {
-  alert("Welcome to Cameron's Bakery. Please take our fun Quiz!");
+jQuery(document).ready(function($) {
+    $('.navbar-header').click(function(){
+        alert("Welcome to Cameron's Bakery. Please take our fun Quiz!");
+    });
 });
